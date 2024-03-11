e7651d090fac04ef8ef27c678975a722aa1c7bd4
Willie Jones <williejones432@yahoo.com>
2024-03-11 11:47:13 -0500

    Added page files, add bg images, and started login page layout

---
+++ client/src/pages/login/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+      <div className='w-full p-6 rounded-lg shadow-md bg-transparent bg-clip-padding backdrop-filter backdrop-blur-lg'>
+        <h1 className='text-3xl font-semibold text-center text-gray-400'>Login <span className='text-blue-500'>ChatApp</span></h1>
+      </div>
+    </div>
+  )
+}
