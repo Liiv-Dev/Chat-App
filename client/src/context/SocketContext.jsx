efe4323ff7a7da40b260f865f0cd7bc8bed91623
Willie Jones <williejones432@yahoo.com>
2024-03-18 10:50:58 -0500

    socket url added

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io('http://localhost:3001', {
+      const socket = io('https://convo-on6g.onrender.com', {
         query: {
           userId: authUser._id
         }
