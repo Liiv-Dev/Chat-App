30360a0d7ca74f0b530b7ee8f82e6d8ddcf383f6
Willie Jones <williejones432@yahoo.com>
2024-03-18 10:37:34 -0500

    adds to packagejson for build

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 import authRoutes from './routes/authRoutes.js';
 import messageRoutes from './routes/messageRoutes.js';
@@ -10,6 +11,7 @@ import connection from './config/connection.js';
 import { app, server } from './socket/socket.js';
 
 const PORT = process.env.PORT || 3001;
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -17,16 +19,21 @@ dotenv.config();
 app.use(express.json()); // to parse the incoming requests with JSON payloads
 app.use(cookieParser()); // to parse the incoming cookies
 
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
+app.use('/api/users', userRoutes);
 
 //app.get('/', (req, res) => {
 //  // root route 
 //  res.send('YOOOO');
 //})
 
-// Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/messages', messageRoutes);
-app.use('/api/users', userRoutes);
+// Serve static assets if in production
+app.use(express.static(path.join(__dirname, '/client/dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/dist/index.html'));
+});
 
 server.listen(PORT, () => {
   connection()
